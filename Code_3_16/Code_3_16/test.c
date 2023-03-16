fd85c9d90256fd62e17801bccace72a228487480
Chen <87429954+ChenJHchen@users.noreply.github.com>
2023-03-16 21:16:02 +0800

    Add files via upload

---
+++ Code_3_16/Code_3_16/test.c
@@ -0,0 +1,30 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//int ADD(int x, int y)
+//{
+//	int z = 0;
+//	z = x + y;
+//	return z;
+//}
+//int main()
+//{
+//	int num1 = 0;
+//	int num2 = 0;
+//	scanf("%d%d", &num1, &num2);
+//	int sum = ADD(num1, num2);
+//	printf("ADD is %d\n", sum);
+//	return 0;
+//}
+
+
+int main()
+{
+	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+	int i = 0;
+	while (i < 10)
+	{
+		printf("%d ", arr[i]);
+		i++;
+	}
+	return 0;
+}
