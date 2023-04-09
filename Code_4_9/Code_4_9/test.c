b16633b2c78f8d81a4a28a172a2d56a7dd6c2e09
Chen <87429954+ChenJHchen@users.noreply.github.com>
2023-04-09 21:34:38 +0800

    Add files via upload

---
+++ Code_4_9/Code_4_9/test.c
@@ -0,0 +1,42 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+//int main()
+//{
+//	int line = 0;
+//	printf("加入比特\n");
+//	while(line<20000)
+//	{
+//		printf("写代码:%d\n",line);
+//		line++;
+//		
+//	}
+//	if (line >= 20000)
+//	{
+//		printf("好offer\n");
+//	}
+//	else
+//	{
+//		printf("继续加油\n");
+//	}
+//	return 0;
+//}
+
+
+//int ADD(int x, int y)
+//{
+//	/*return(x + y);*/
+//	int z = 0;
+//	z = x + y;
+//	return z;
+//}
+//int main()
+//{
+//	int num1 = 0;
+//	int num2 = 0;
+//	scanf("%d %d", &num1, &num2);
+//	int sum = ADD(num1,num2);
+//	printf("%d\n", sum);
+//
+//
+//	return 0;
+//}
