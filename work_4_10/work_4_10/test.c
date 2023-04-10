77154d431130d88830b4596a502ff86ea8224567
Chen <87429954+ChenJHchen@users.noreply.github.com>
2023-04-10 14:46:58 +0800

    Add files via upload

---
+++ work_4_10/work_4_10/test.c
@@ -0,0 +1,79 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+//int main()
+//{
+//	printf("Name    Age   Gender\n");
+//	printf("---------------------\n");
+//	printf("Jack    18    Man\n");
+//	return 0;
+//}
+
+//int main()
+//{
+//	printf("I lost my cellphone\n");
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int a = 40;
+//	int c = 212;
+//	printf("%d\n", (-8 + 22) * a - 10 + c/2);
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	scanf("%d %d",& a, &b);
+//	if (a > b)
+//	{
+//		printf("%d\n", a);
+//	}
+//	else
+//	{
+//		printf("%d\n", b);
+//	}
+//	return 0;
+//}
+
+//int max(int x, int y)
+//{
+//	if (x > y)
+//	{
+//		return x;
+//	}
+//	else
+//	{
+//		return y;
+//	}
+//		
+//}
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	scanf("%d %d", &a, &b);
+//	int r = max(a, b);
+//	printf("%d\n", r);
+//	return 0;
+//}
+
+//
+//int main()
+//{
+//	int x = 0;
+//	int y = 0;
+//	scanf("%d", &x);
+//	if (x > 0)
+//		y = 1;
+//	else if (x == 0)
+//		y = 0;
+//	else
+//		y = 1;
+//	printf("%d\n", y);
+//	return 0;
+//}
