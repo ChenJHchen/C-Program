a1e559a91b3ad9ebe0f221c0bdbec6cf3190d233
Chen <87429954+ChenJHchen@users.noreply.github.com>
2023-04-13 17:24:03 +0800

    Add files via upload

---
+++ Code_4_13/Code_4_13/test.c
@@ -0,0 +1,81 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include<stdio.h>
+// 
+
+
+//int main()
+//{
+//	int num = 0;
+//	scanf("%d", &num);
+//	if (num % 2 == 1)
+//	{
+//		printf("ÆæÊý\n");
+//	}
+//	else
+//	{
+//		printf("NO\n");
+//	}
+//	return 0;
+//}
+
+//
+//
+//int main()
+//{
+//	int i = 1;
+//	while (i <= 100)
+//	{
+//	
+//			printf("%d ", i);
+//		i+=2;
+//	}
+//	return 0;
+//}
+
+
+
+//int main()
+//{
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int i = 1;
+//	while (i <= 10)
+//	{
+//		i++;
+//		if (5 == i)
+//			continue;
+//		printf("%d ", i);
+//		
+//	}
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	char password[20] = { 0 };
+//	printf("ÇëÊäÈëÃÜÂë:>");
+//	scanf("%s", password);
+//	int ch = 0;
+//	while ((ch = getchar()) != '\n')
+//	{
+//		;
+//	}
+//	printf("ÇëÈ·ÈÏÃÜÂë(Y/N):>");
+//	int ret = getchar();
+//	if ('Y' == ret)
+//	{
+//		printf("YES\n");
+//	}
+//	else
+//	{
+//		printf("NO\n");
+//
+//	}
+//	return 0;
+//}
