9d6b32a1589037095b40ba0a7cdd8f1f4ce4c96e
Chen <87429954+ChenJHchen@users.noreply.github.com>
2023-04-13 21:56:17 +0800

    Add files via upload

---
+++ work_4_13/work_4_13/test.c
@@ -0,0 +1,139 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+//int main()
+//{
+//	int weight = 0;
+//	int high = 0;
+//	scanf("%d %d", &weight, &high);
+//	double BMI = 0;
+//	BMI = weight / ((high/100.0) * (high/100.0));
+//	printf("%.2lf\n", BMI);
+//
+//	return 0;
+//}
+
+//#include<math.h>
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	int c = 0;
+//	scanf("%d %d %d", &a, &b, &c);
+//	float len = 0;
+//	len = a + b + c;
+//	float p = (a + b + c) / 2.0;
+//	float area = sqrt(p * (p - a) * (p - b) * (p - c));
+//	
+//	printf("len=%.2lf area=%.2lf\n", len,area);
+//	return 0;
+//}
+
+
+
+
+
+//int main()
+//{
+//	double r = 0.0;
+//	double pi = 3.1415926;
+//	scanf("%lf", &r);
+//	printf("%.3lf\n", (4.0 / 3) * pi * r * r * r);
+//
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int score1 = 0;
+//	int score2 = 0;
+//	int score3 = 0;
+//	scanf("%d%d%d", &score1, &score2, &score3);
+//	printf("score1=%d,score2=%d,score3=%d\n", score1, score2, score3);
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int i = 0;
+//	int score[3] = { 0 };
+//	for (i = 0; i < 3; i++)
+//	{
+//		scanf("%d", &score[i]);
+//	}
+//	printf("score1=%d,score2=%d,score3=%d\n", score[0], score[1], score[2]);
+//	return 0;
+//}
+
+
+
+//int main()
+//{
+//	int i = 0;
+//	for (i = 10000; i <= 99999; i++)
+//	{
+//		int j = 0;
+//		int sum = 0;
+//		for (j = 10; j <= 10000; j*= 10)
+//		{
+//			sum += (i /j) * (i % j);
+//		}
+//		if (sum == i)
+//			printf("%d ", i);
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	scanf("%d %d", &a, &b);
+//	if (a > 100 && b>100)
+//	{
+//		a %= 100;
+//		b %= 100;
+//		int sum = a + b;
+//		printf("%d\n", sum%100);
+//	}
+//	else
+//	{
+//		int sum = a + b;
+//		printf("%d\n", sum % 100);
+//	}
+//	
+//	return 0;
+//}
+
+//int main()
+//{
+//	double d = 0;
+//	scanf("%lf", &d);
+//	int n = (int)d;
+//	printf("%d\n", n % 10);
+//
+//	return 0;
+//}
+//
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	printf("%d\n", n % 10);
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int age = 0;
+//	long long  second = 0;
+//	scanf("%d", &age);
+//	second = age * 3.156e7;
+//	printf("%lld\n", second);
+//	
+//
+//
+//	return 0;
+//}
