152ea7870c0b085a3fc5c31df3993510cf6f41ed
Chen <87429954+ChenJHchen@users.noreply.github.com>
2023-04-14 13:30:34 +0800

    Add files via upload

---
+++ work_4_14/work_4_14/test.c
@@ -0,0 +1,46 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+//int main()
+//{
+//	int arr[4] = { 0 };
+//	int i = 0;
+//	while (i < 4)
+//	{
+//		scanf("%d", &arr[i]);
+//		i++;
+//	}
+//	int max = arr[0];//假设第一个是最大值
+//	i = 1;
+//	while (i < 4)
+//	{
+//		if (arr[i] > max)
+//		{
+//			max = arr[i];
+//		}
+//		i++;
+//	}
+//	printf("%d\n", max);
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int i = 1;
+//	int n = 0;
+//	int max = 0;
+//	scanf("%d", &max);
+//	while (i < 4)
+//	{
+//		scanf("%d", &n);
+//		if (n > max)
+//		{
+//			max = n;
+//			
+//		}
+//		i++;
+//		
+//	}
+//	printf("%d\n", max);
+//	return 0;
+//}
