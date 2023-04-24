49c928e13d7b2569364aa25a4b62ae5ead4e8a1a
Chen <87429954+ChenJHchen@users.noreply.github.com>
2023-04-24 18:28:28 +0800

    Add files via upload

---
+++ code_4_24/code_4_24/test.c
@@ -0,0 +1,185 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+//void Swap(int* px,int* py)
+//{
+//	int tmp = *px;
+//	*px = *py;
+//	*py = tmp;
+//}
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	int c = 0;
+//	int tmp = 0;
+//	scanf("%d %d %d", &a, &b, &c);
+//	if (a < b)
+//	{
+//		Swap(&a, &b);
+//	}
+//	if (a < c)
+//	{
+//		Swap(&a, &c);
+//
+//
+//	}
+//	if (b < c)
+//	{
+//		Swap(&b, &c);
+//
+//	}
+//	printf("%d %d %d\n", a, b, c);
+//
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int i = 3;
+//	while (i <= 100)
+//	{
+//		printf("%d ", i);
+//			i += 3;
+//	}
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	scanf("%d %d", &a, &b);
+//	int min = (a < b) ? a : b;
+//	int m = min;
+//	while (1)
+//	{
+//		if (a % m == 0 && b % m == 0)
+//		{
+//			break;
+//		}
+//		m--;
+//	}
+//	printf("%d\n", m);
+//	return 0;
+//}
+
+
+
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	int c = 0;
+//
+//	scanf("%d %d", &a, &b);
+//	while (c=a%b)
+//	{
+//		
+//		a = b;
+//		b = c;
+//	}
+//	printf("%d ", b);
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int i = 0;
+//	int count = 0;
+//	for (i = 1; i <= 100; i++)
+//	{
+//		if (i % 10 == 9)
+//			count++;
+//
+//			if (i / 10 == 9)
+//				count++;
+//	}
+//	printf("%d", count);
+//	return 0;
+//
+//}
+
+
+//int main()
+//{
+//	int i = 0;
+//	double sum = 0;
+//	int flag = 1;
+//	for (i = 1; i <= 100; i++)
+//	{
+//		sum = sum + flag*(1.0 / i);
+//		flag = -flag;
+//	}
+//	printf("%lf\n", sum);
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+//	int max = arr[0];
+//	int i = 0;
+//	for (i = 1; i <= 10; i++)
+//	{
+//		if (arr[i] > max)
+//		{
+//			max = arr[i];
+//		}
+//	}
+//	printf("%d\n", max);
+//	return 0;
+//}
+
+
+
+//int main()
+//{
+//	int i = 0;
+//	for (i = 1; i <= 9; i++)
+//	{
+//		int j = 0;
+//		for (j = 1; j <= i; j++)
+//		{
+//			printf("%d*%d=%2d ", i, j, i * j);
+//		
+//		}
+//		printf("\n");
+//	}
+//	
+//
+//	return 0;
+//}
+//void print_table(int x)
+//{
+//	int i = 0;
+//		for (i = 1; i <= x; i++)
+//		{
+//			int j = 0;
+//			for (j = 1; j <= i; j++)
+//			{
+//				printf("%d*%d=%2d ", i, j, i * j);
+//			
+//			}
+//			printf("\n");
+//		}
+//
+//}
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	print_table(n);
+//	return 0;
+//}
+
+
+int main()
+
+{
+
+	return 0;
+}
