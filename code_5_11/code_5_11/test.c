ac2e4c5170f50271767abcb544c72ee1c6ed60d4
Chen <87429954+ChenJHchen@users.noreply.github.com>
2023-05-11 17:59:51 +0800

    Add files via upload

---
+++ code_5_11/code_5_11/test.c
@@ -0,0 +1,79 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include<stdio.h>
+//int main()
+//{
+//	int age = 0;
+//	scanf("%d", &age);
+//	if (age < 18)
+//	{
+//		printf("少年\n");
+//	}
+//	else if (age >= 18 && age < 30)
+//	{
+//		printf("青年\n");
+//	}
+//	else if (age >= 30 && age < 50)
+//	{
+//		printf("中年\n");
+//	}
+//	else if (age >= 50 && age < 80)
+//	{
+//		printf("老年\n");
+//	}
+//	else
+//	{
+//		printf("寿星\n");
+//	}
+//	return 0;
+//}
+//
+//int main()
+//{
+//	int day = 0;
+//	scanf("%d", &day);
+//	switch (day)
+//	{
+//	case 1:
+//		printf("星期一\n");
+//		break;
+//	case 2:
+//		printf("星期二\n");
+//		break;
+//	case 3:
+//		printf("星期3\n");
+//		break;
+//	case 4:
+//		printf("星期4\n");
+//		break;
+//	case 5:
+//		printf("星期5\n");
+//		break;
+//	case 6:
+//		printf("星期6\n");
+//	case 7:
+//		printf("星期7\n");
+//		break;
+//	default:
+//		printf("err");
+//		break;
+//
+//	}
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = 20;
+//	if (*(char*)&a == 1)
+//	{
+//		printf("小端\n");
+//
+//	}
+//	else
+//	{
+//		printf("大端\n");
+//	}
+//	return 0;
+//}
