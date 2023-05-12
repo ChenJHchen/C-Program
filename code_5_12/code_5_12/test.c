2176ead39acec27e2a70bea34f480d7b37cdb504
Chen <87429954+ChenJHchen@users.noreply.github.com>
2023-05-12 17:41:02 +0800

    Add files via upload

---
+++ code_5_12/code_5_12/test.c
@@ -0,0 +1,46 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include<stdio.h>
+//void move_odd_even(int arr[], int sz)
+//{
+//	int left = 0;
+//	int rigft = sz - 1;
+//	while (left<rigft)
+//	{
+//		while ((left<rigft) && (arr[left] % 2 == 1))
+//		{
+//			left++;
+//		}
+//		while (((left < rigft)) && (arr[rigft] % 2 == 0))
+//		{
+//			rigft--;
+//		}
+//		if (left < rigft)
+//		{
+//			int tmp = arr[left];
+//			arr[left] = arr[rigft];
+//			arr[rigft] = tmp;
+//			left++;
+//			rigft--;
+//		}
+//	}
+//}
+//int main()
+//{
+//	int arr[10] = { 0 };
+//	int i = 0;
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	for (i = 0; i < sz; i++)
+//	{
+//		scanf("%d", arr + i);
+//	}
+//	move_odd_even(arr, sz);
+//	for (i = 0; i < sz; i++)
+//	{
+//		printf("%d ", arr[i]);;
+//	}
+//
+//	return 0;
+//}
+
+
