e4bb913a992e178cc8a72d56e07ecceb5d9dafb2
Chen <87429954+ChenJHchen@users.noreply.github.com>
2023-06-17 21:16:52 +0800

    Add files via upload

---
+++ code_6_17/code_6_17/test.c
@@ -0,0 +1,30 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include<stdio.h>
+#include<string.h>
+#include<assert.h>
+void* my_memcpy(void* dest,const void* src,size_t num)
+{
+	assert(dest && src);
+	void* ret = dest;
+	while (num--)
+	{
+		*(char*)dest = *(char*)src;
+		dest = (char*)dest + 1;
+		src = (char*)src + 1;
+
+	}
+	return ret;
+}
+int main()
+{
+	int arr1[] = { 1,2,3,4,5,6,7,8,9,10 };
+	int arr2[20] = { 0 };
+	my_memcpy(arr2, arr1,20);
+	int i = 0;
+	for (i = 0; i < 5; i++)
+	{
+		printf("%d ", arr2[i]);
+	}
+	return 0;
+}
