f0a4028ffd75a08ddf4cb3921a2aa431125de88e
Chen <87429954+ChenJHchen@users.noreply.github.com>
2023-08-05 23:44:35 +0800

    Add files via upload

---
+++ code_8_5/code_8_5/test.c
@@ -0,0 +1,33 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+//void test()
+//{
+//	int a = 1;
+//	a++;
+//	printf("%d ", a);
+//}
+//int main()
+//{
+//	int i = 0;
+//	while (i < 10);
+//	{
+//		test();
+//		i++;
+//	}
+//	return 0;
+//}
+//
+//
+
+
+
+int main()
+{
+	int i = 0;
+	while (i < 10)
+	{
+		i++;
+		printf("%d ", i);
+	}
+	return 0;
+}
